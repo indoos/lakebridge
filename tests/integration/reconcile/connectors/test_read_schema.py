d0e31df8bb9e4f853837538809058a6175110f05
M Abulazm <mohamed.abulazm@databricks.com>
2025-10-10 15:29:18 +0200

    fix databricks schema query (#2091)
    <!-- REMOVE IRRELEVANT COMMENTS BEFORE CREATING A PULL REQUEST -->
    ## Changes
    <!-- Summary of your changes that are easy to understand. Add
    screenshots when necessary, they're helpful to illustrate the before and
    after state -->
    ### What does this PR do?
    Fix databricks reconcile schema queries
    
    ### Tests
    1. added test for global views
    2. testing tables within catalogs is manually tested on a sandbox
    databricks
    3. need to add test for hive metastore but this requires modifications
    to our test standalone spark setup

--- tests/integration/reconcile/connectors/test_read_schema.py
+++ tests/integration/reconcile/connectors/test_read_schema.py
@@ -2,6 +2,7 @@ from unittest.mock import create_autospec
 
 import pytest
 
+from databricks.labs.lakebridge.reconcile.connectors.databricks import DatabricksDataSource
 from databricks.labs.lakebridge.reconcile.connectors.tsql import TSQLServerDataSource
 from databricks.labs.lakebridge.transpiler.sqlglot.dialect_utils import get_dialect
 
@@ -30,4 +31,16 @@ def test_tsql_server_read_schema_happy(mock_spark):
     connector = TSQLServerDataSourceUnderTest(get_dialect("tsql"), mock_spark, mock_ws, "my_secret")
 
     columns = connector.get_schema("labs_azure_sandbox_remorph", "dbo", "Employees")
+    assert columns
+
+
+def test_databricks_read_schema_happy(mock_spark):
+    mock_ws = create_autospec(WorkspaceClient)
+    connector = DatabricksDataSource(get_dialect("databricks"), mock_spark, mock_ws, "my_secret")
+
+    mock_spark.sql("CREATE DATABASE IF NOT EXISTS my_test_db")
+    mock_spark.sql("CREATE TABLE IF NOT EXISTS my_test_db.my_test_table (id INT, name STRING) USING parquet")
+    df = mock_spark.sql("SELECT * FROM my_test_db.my_test_table")
+    df.createGlobalTempView("my_global_test_view")
+    columns = connector.get_schema(None, "global_temp", "my_global_test_view")
     assert columns
